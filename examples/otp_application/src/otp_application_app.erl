c8b0b80eb50a78940987c946d2483c3399827bd1
Fred Dushin <fred@dushin.net>
2023-11-18 09:23:14 -0500

    Add support for OTP application framework in AtomVM
    Signed-off-by: Fred Dushin <fred@dushin.net>

---
+++ examples/otp_application/src/otp_application_app.erl
@@ -0,0 +1,26 @@
+%%
+%% Copyright (c) 2023 <fred@dushin.net>
+%% All rights reserved.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%
+-module(otp_application_app).
+
+-export([start/2, stop/1]).
+
+start(StartType, StartArgs) ->
+    io:format("Starting otp_application_app with start type ~p and start args ~p ...~n", [StartType, StartArgs]),
+    otp_application_sup:start(StartArgs).
+
+stop(_State) ->
+    ok.
